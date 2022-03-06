12d34439204c3c5ec0fd79cff4a175e9345515cb
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-03-06 12:35:09 +0530

    Added config client dependency and eureka server annotation

--- src/main/java/com/cts/OstockEurekaServerApplication.java
+++ src/main/java/com/cts/OstockEurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.cts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class OstockEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OstockEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OstockEurekaServerApplication.class, args);
+    }
 
 }
