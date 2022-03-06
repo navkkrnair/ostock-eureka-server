081381b1900fab5935e11c1a8383a1322ef01d27
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-03-06 10:27:02 +0530

    First commit

---
+++ src/main/java/com/cts/OstockEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstockEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstockEurekaServerApplication.class, args);
+	}
+
+}
